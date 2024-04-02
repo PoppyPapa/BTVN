bed3d0720c62a2a8b0cace2a8d3c06c1732ee9d8
PoppyPapa <165580366+PoppyPapa@users.noreply.github.com>
2024-04-02 09:04:45 +0700

    ok2

--- BT2/Program.cs
+++ BT2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +6,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BT1
+namespace BT2
 {
 
     internal class Program
@@ -23,12 +23,12 @@ namespace BT1
                     Console.OutputEncoding = Encoding.UTF8;
                     Console.WriteLine("\n");
                     Console.WriteLine("***MENU***");
-                    Console.WriteLine("**********************");
-                    Console.WriteLine("1. Thêm mới.");
-                    Console.WriteLine("2. Hiển thị danh sách.");
+                    Console.WriteLine("*******************************");
+                    Console.WriteLine("1. Thêm học sinh mới.");
+                    Console.WriteLine("2. Hiển thị danh sách học sinh.");
                     Console.WriteLine("3. Tìm kiếm.");
                     Console.WriteLine("4. Kết thúc.");
-                    Console.WriteLine("**********************");
+                    Console.WriteLine("*******************************");
                     Console.WriteLine("\nChọn: ");
 
                     int selection = 0;
@@ -49,7 +49,7 @@ namespace BT1
                     switch (selection)
                     {
                         case 1:
-                            themsach();
+                            themhocsinh();
                             break;
                         case 2:
                             hienthi();
@@ -67,65 +67,75 @@ namespace BT1
             Console.ReadKey();
         }
 
-        //khai báo danh mục sách 
-        public struct danhmucsach
+        //khai báo danh sách học sinh
+        public struct danhsachhocsinh
         {
-            public string tensach;
-            public string tacgia;
-            public int namxuatban;
+            public string hoten;
+            public int namsinh;
+            public float diemtrungbinh;
         }
-        static List<danhmucsach> thuviensach = new List<danhmucsach>();
+        static List<danhsachhocsinh> hocsinh  = new List<danhsachhocsinh>();
 
-        //thêm đầu sách
-        static void themsach()
+        //thêm học sinh
+        static void themhocsinh()
         {
             Console.OutputEncoding = Encoding.UTF8;
-            danhmucsach sachmoi = new danhmucsach();
-            Console.WriteLine("Tên đầu sách: ");
-            sachmoi.tensach = Console.ReadLine();
+            danhsachhocsinh hocsinhmoi = new danhsachhocsinh();
+            Console.WriteLine("Họ và tên: ");
+            hocsinhmoi.hoten = Console.ReadLine();
 
-            Console.WriteLine("Tác giả: ");
-            sachmoi.tacgia = Console.ReadLine();
-
-            Console.WriteLine("Năm xuất bản: ");
-            bool isNum = int.TryParse(Console.ReadLine(), out sachmoi.namxuatban);
+            Console.WriteLine("Năm sinh: ");
+            bool isNum = int.TryParse(Console.ReadLine(), out hocsinhmoi.namsinh);
             for (int i = 0; i < 1; i--)
             {
-                if (isNum == true && sachmoi.namxuatban > 0 && sachmoi.namxuatban < 2025)
+                if (isNum == true && hocsinhmoi.namsinh > 1990 && hocsinhmoi.namsinh < 2025)
                     break;
                 else
                 {
-                    Console.WriteLine("Nhập lại năm xuất bản: ");
-                    isNum = int.TryParse(Console.ReadLine(), out sachmoi.namxuatban);
+                    Console.WriteLine("Nhập lại năm sinh: ");
+                    isNum = int.TryParse(Console.ReadLine(), out hocsinhmoi.namsinh);
                 }
             }
-            thuviensach.Add(sachmoi);
+
+            Console.WriteLine("Điểm trung bình: ");
+            bool isFloat = float.TryParse(Console.ReadLine(), out hocsinhmoi.diemtrungbinh);
+            for (int i = 0; i < 1; i--)
+            {
+                if (isFloat == true && hocsinhmoi.diemtrungbinh >= 0.0 && hocsinhmoi.diemtrungbinh <= 10.0)
+                    break;
+                else
+                {
+                    Console.WriteLine("Nhập lại điểm trung bình: ");
+                    isNum = float.TryParse(Console.ReadLine(), out hocsinhmoi.diemtrungbinh);
+                }
+            }
+            hocsinh.Add(hocsinhmoi);
         }
 
-        //hiển thị danh mục sách
+        //hiển thị danh sách học sinh
         static void hienthi()
         {
-            for (int i = 1; i <= thuviensach.Count; i++)
+            for (int i = 1; i <= hocsinh.Count; i++)
             {
                 Console.Write("STT {0} - ", i);
-                thuviensach.ForEach(hienthi => Console.WriteLine($"Tiêu đề: {hienthi.tensach}, Tác giả: {hienthi.tacgia}, Năm xuất bản: {hienthi.namxuatban}"));
+                hocsinh.ForEach(hienthi => Console.WriteLine($"Họ và tên: {hienthi.hoten}, Năm sinh: {hienthi.namsinh}, Điểm trung bình: {hienthi.diemtrungbinh}"));
             }
         }
 
         //tìm kiếm thông tin
         static void timkiem()
         {
-            Console.WriteLine("Nhập tiêu đề cần tìm");
+            Console.WriteLine("Nhập tên cần tìm");
             string tukhoa = Console.ReadLine();
-            foreach (var tieude in thuviensach)
+            foreach (var hoten in hocsinh)
             {
-                if (tukhoa == tieude.tensach)
+                if (tukhoa == hoten.hoten)
                 {
-                    Console.WriteLine($"Tiêu đề: {tieude.tensach}, Tác giả: {tieude.tacgia}, Năm xuất bản: {tieude.namxuatban}");
+                    Console.WriteLine($"Họ và tên: {hoten.hoten}, Năm sinh: {hoten.namsinh}, Điểm trung bình: {hoten.diemtrungbinh}");
                 }
                 else
                 {
-                    Console.WriteLine("Không tìm thấy sách bạn cần!");
+                    Console.WriteLine("Không tìm thấy tên bạn cần!");
 
                 }
             }
